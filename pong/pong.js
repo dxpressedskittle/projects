5be69eaa829dfe55019e1e2ff81f251610fda6bf
DxpressedSkittle <hinuto026@gmail.com>
2025-05-27 13:00:21 +0000

    Pong changes

--- pong/pong.js
+++ pong/pong.js
@@ -1,38 +1,83 @@
+let pScore = document.getElementById("pScore")
+let p2Score = document.getElementById("p2Score")
 let display = document.getElementById("display")
 
 
 
 document.addEventListener('keydown', function(){
     if (event.key == "h") {
-        startGame()
+        startGame();
     }
 
 })
 
 function startGame() {
     display.innerHTML =""
+    ball.style.visibility = "visible"
+    animate();
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 const ball = document.getElementById("ball");
 const box = document.getElementById("gameDisplay")
-            let boxLength = box.offsetWidth
-            let x = 0;
-            let y = 0;
-            let xSpeed = 5;
-            let ySpeed = 5;
+
+
+            let boxLength = 600
+            let x = 370;
+            let y = 300;
+            let xSpeed = 3.5;
+            let ySpeed = 0;
+            ball.style.left = x + "px";
+            ball.style.top = y + "px";
+            ball.style.visibility = "hidden";
+            
+             /* function getRandomDirection() {
+            if (getRandomInt(0, 3) == 0) {
+                xSpeed = -xSpeed; 
+             } else if (getRandomInt(0, 3) == 1) {
+                ySpeed = -ySpeed;
+             } else if (getRandomInt(0, 3) == 2) {
+                ySpeed = ySpeed;
+             } else if (getRandomInt(0, 3) == 3) {
+                xSpeed = xSpeed;
+             }}
+                */
+               function getRandomDirection() {
+            const angle = Math.random() * 2 * Math.PI;
+            return {
+            x: Math.cos(angle),
+            y: Math.sin(angle),
+            };
+             }
+            
+                
+
             function animate() {
             x += xSpeed;
             y += ySpeed;
-            if (x + 50 > 600 || x < 0) {
-               xSpeed = -xSpeed;
+            if (x + 50 > 800 || x < 0) {
+               resetBall();
             }
-            if (y + 50 > window.innerHeight || y < 0) {
+            if (y + 50 > 650 || y < 0) {
                ySpeed = -ySpeed;
             }
             ball.style.left = x + "px";
             ball.style.top = y + "px";
             requestAnimationFrame(animate);
          }
-        animate();
+
+
+         function resetBall() {
+            getRandomDirection();
+            x = 370;
+            y = 300;
+
+         }
+
+    
--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// File is now empty as glow is handled by CSS only.
+// Global functions 
+
+
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
